afdb678adf22639bbb83c015c312180466e70e2a
Adam Giunta <amgiunta.2016@mymail.becker.edu>
2019-05-08 14:18:37 -0400

    Commented a shit ton of stuff

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,32 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// The Game Master script will be used to manage the game scene and game data.
+/// </summary>
 public class GameMaster : MonoBehaviour {
-
+    // The current working Game Master instance.
     public static GameMaster gameMaster;
-
-
+    
     private void Awake()
     {
+        #region Singleton
         if (gameMaster)
         {
             Destroy(gameMaster.gameObject);
             gameMaster = this;
         }
         else { gameMaster = this; }
+        #endregion
     }
 
+    /// <summary>
+    /// Re-loads the current open unity scene.
+    /// </summary>
     public void RestartLevel() {
+        // Create int build index that is the build index of the current active scene.
         int buildIndex = SceneManager.GetActiveScene().buildIndex;
 
+        // Change the scene asynchronously.
         StartCoroutine(LevelChangeTransition(buildIndex, 1f));
     }
 
+    /// <summary>
+    /// Asynchronously load and transition from one scene to another.
+    /// </summary>
+    /// <param name="sceneIndex">The build index of the scene to load.</param>
+    /// <param name="delay">The delay in seconds to wait for before loading the scene.</param>
+    /// <returns>An enumerator</returns>
     private IEnumerator LevelChangeTransition(int sceneIndex, float delay) {
+        // Create Animator camera animator that is the Animator attached to the object in the scene with the tag "MainCamera".
         Animator cameraAnimator = GameObject.FindGameObjectWithTag("MainCamera").GetComponentInChildren<Animator>();
+        // Set the 'Exit' trigger on the camera animator.
         cameraAnimator.SetTrigger("Exit");
+        // Wait for the duration of delay
         yield return new WaitForSeconds(delay);
 
+        // Use the Scene Manager to load the scene with the build index scene index.
         SceneManager.LoadScene(sceneIndex);
     }
 }
--- Assets/Scripts/HoldableObject.cs
+++ Assets/Scripts/HoldableObject.cs
@@ -3,63 +3,108 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Collider2D))]
 /// <summary>
 /// Wrapper for gameobjects that are holdable.
 /// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class HoldableObject : MonoBehaviour {
 
-
+    /// <summary>
+    /// Align the rotation of this object with the rotation of the player.
+    /// </summary>
+    [Tooltip("Align the rotation of this object with the rotation of the player.")]
     public bool orientWithPlayer;
+    /// <summary>
+    /// Should the gravity of this object always be inverted?
+    /// </summary>
+    [Tooltip("Should the gravity of this object always be inverted?")]
     public bool invertUp;
 
+    /// <summary>
+    /// The rigidbody attached to this object
+    /// </summary>
+    [HideInInspector]
     new public Rigidbody2D rigidbody;
+    /// <summary>
+    /// The collider attached to this object.
+    /// </summary>
+    [HideInInspector]
     new public Collider2D collider;
 
+    /// <summary>
+    /// A reference to the current acting player script in the scene.
+    /// </summary>
     private PlayerNetwork player;
+    /// <summary>
+    /// The relative up direction of this object.
+    /// </summary>
     private Vector3 up;
 
     protected virtual void Start () {
+        // Find the player in the scene and store a reference to it.
         FindPlayer();
+        // Find the rigidbody component attached to this object.
         rigidbody = GetComponent<Rigidbody2D>();
+        // Find the collider object attached to this object.
         collider = GetComponent<Collider2D>();
 
+        // Initialize up to be the default up vector.
         up = Vector3.up;
     }
 
     protected virtual void FixedUpdate() {
+        // Apply force of gravity along the relative up vector.
         ApplyGravity();
     }
 
     protected virtual void Update() {
+        // Set the rotation of this object accordingly.
         Orient();
     }
 
+    /// <summary>
+    /// Sets the rotation of this object. Based on value of orientWithPlayer and invertUp.
+    /// </summary>
     protected virtual void Orient() {
+        // If not inverted, and orientWithPlayer is true, set the up vector to be the up vector of the player.
         if (orientWithPlayer && player && !invertUp) { up = player.transform.up; }
+        // Or, if orientWithPlayer is true, and invertUp is true, set the relative up vector to be the negative up vector of the player.
         else if (orientWithPlayer && player && invertUp) { up = -player.transform.up; }
+        // Or else,
         else {
+            // If inverting, set the relative up vector to be the negative default up vector.
             if (invertUp) { up = -Vector3.up; }
+            // Otherwise, the up vector is the default up vector.
             else { up = Vector3.up; }
         }
     }
 
+    /// <summary>
+    /// Apply to force of gravity along the relative up vector.
+    /// </summary>
     protected virtual void ApplyGravity() {
+        // Create Vector direction that is the negative relative up vector
         Vector3 direction = -up;
+        // Multiply the direction by: the negative force of gravity multiplied by the mass of this object.
         direction *= (-Physics2D.gravity.y * rigidbody.mass);
+        // Add direction to this object as a force.
         rigidbody.AddForce(direction);
     }
 
+    /// <summary>
+    /// Locates the player within the scene and saves a reference to the script.
+    /// </summary>
     public virtual void FindPlayer() {
+        // Set the player to the Player Network on the object in the scene with the tag "Player".
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerNetwork>();
 
+        // If the player is not found, throw an error.
         if (!player) { Debug.LogError("Could not locate player! Check to see if the player is in the scene, tagged as 'Player', and has the PlayerNetwork (or some derivative behavior) attached.", this); }
     }
 
     void OnDrawGizmos() {
-        Gizmos.color = Color.yellow;
-
+        // If the game is playing, Draw a ray from the position of this object with a magnitude of this object's velocity.
         if (Application.isPlaying)
             Debug.DrawRay(transform.position, rigidbody.velocity);
     }
