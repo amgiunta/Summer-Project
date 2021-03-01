b0b4b4b511a808e67bc07a0ff95536298fc5971e
Adam Giunta <amgiunta.2016@mymail.becker.edu>
2021-02-28 22:13:09 -0500

    Added basic camera spaces

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    public Transform target;
+    public static CameraFollow mainCamera;
+
+    public enum CameraMode { Fixed, Zoom, Encapsulate, Default };
+    public CameraMode cameraMode = CameraMode.Default;
+
+    public List<Transform> targets;
     public PlayerNetwork player {
         get {
             if (!_player) {
@@ -17,57 +22,117 @@ public class CameraFollow : MonoBehaviour {
 
     public Vector3 offset;
     public Vector2 predictionAmount;
+    public float targetZoom;
+    public float defaultZoom;
+    public float margin;
 
     public float cameraSpeed;
+    public float zoomSpeed;
     public float rotationSpeed;
     public bool prediction;
 
-    private PlayerNetwork _player;
+    //[HideInInspector]
+    public CameraSpace currentSpace;
 
-	// Use this for initialization
-	void Start () {
+    private PlayerNetwork _player;
+    [HideInInspector]
+    public Bounds targetBounds;
+    public Camera cam;
+
+    private void Awake()
+    {
+        if (mainCamera) {
+            Destroy(mainCamera.gameObject);
+        }
+
+        mainCamera = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponentInChildren<Camera>();
+
+        ResetZoom();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         MoveCamera();
+
+        targetBounds = TargetBounds();
 	}
 
-    public void MoveCamera(out Vector3 position) {
+    public void MoveCamera() {
 
-        if (player) {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward, player.transform.up), Time.deltaTime * rotationSpeed);
+        switch (cameraMode) {
+            
+            case CameraMode.Encapsulate:
+                SetZoom(targetBounds.size.x > targetBounds.size.y ? targetBounds.size.x / 2 : targetBounds.size.y / 2);
+                DefaultMovement();
+                break;
+            case CameraMode.Zoom:
+            case CameraMode.Fixed:
+            default:
+                DefaultMovement();
+                break;
         }
-
-        position = new Vector3(player.transform.position.x, player.transform.position.y, offset.z);
-
-        
-        //if (!player.isFliping)
-        //{
-        //    position = player.transform.position + offset;
-        //    if (prediction && player)
-        //    {
-        //        Vector2 velocity = player.rigidbody.velocity;
-        //        velocity.Scale(predictionAmount);
-        //        position += (Vector3) velocity;
-        //    }
-
-        //}
-        //else {
-        //    transform.forward = player.transform.forward;
-        //    position = new Vector3(player.transform.position.x, player.transform.position.y, offset.z);
-
-        //    transform.position = position;
-        //}
-        
-
-        transform.position = Vector3.Lerp(transform.position, position, cameraSpeed * Time.deltaTime);
-
     }
 
-    public void MoveCamera() {
-        Vector3 position;
-        MoveCamera(out position);
+    public void SetMode(CameraMode mode) {
+        cameraMode = mode;
+    }
+
+    public void SetZoom(float zoom) {
+        targetZoom = zoom + margin;
+    }
+
+    public void ResetZoom() {
+        targetZoom = defaultZoom;
+    }
+
+    private void DefaultMovement() {
+        if (player)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, TargetRotation(), Time.deltaTime * rotationSpeed);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, TargetPosition(), cameraSpeed * Time.deltaTime);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomSpeed);
+    }
+
+    private Quaternion TargetRotation() { 
+        switch (cameraMode)
+        {
+            case CameraMode.Encapsulate:
+            case CameraMode.Zoom:
+            case CameraMode.Fixed:
+            default:
+                return Quaternion.LookRotation(Vector3.forward, player.transform.up);
+        }
+    }
+
+    private Vector3 TargetPosition() {
+        switch (cameraMode)
+        {
+            case CameraMode.Fixed:
+            case CameraMode.Encapsulate:
+                return new Vector3(targetBounds.center.x, targetBounds.center.y, offset.z);
+            case CameraMode.Zoom:
+            default:
+                return new Vector3(player.transform.position.x, player.transform.position.y, offset.z);
+        }
+    }
+
+    private Bounds TargetBounds() {
+        if (targets.Count == 0) { return new Bounds(); }
+
+        Bounds bounds = new Bounds(targets[0].position, Vector3.zero);
+        foreach (Transform target in targets) {
+            if (target == targets[0]) { continue; }
+            bounds.Encapsulate(target.position);
+        }
+
+        return bounds;
     }
 
     private void OnDrawGizmos() {
---
+++ Assets/Scripts/CameraSpace.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class CameraSpace : MonoBehaviour
+{
+
+    public CameraFollow.CameraMode cameraMode;
+
+    public UnityEvent onSpaceEnter;
+    public UnityEvent onSpaceExit;
+
+    public List<Transform> targets;
+    public float zoom;
+
+    new private Collider2D collider;
+
+    private void Start()
+    {
+        switch (cameraMode)
+        {
+            case CameraFollow.CameraMode.Fixed:
+                onSpaceEnter.AddListener(() => {
+                    CameraFollow.mainCamera.SetMode(cameraMode);
+                    CameraFollow.mainCamera.targets.Add(transform);
+                    CameraFollow.mainCamera.SetZoom(zoom);
+                });
+                break;
+            case CameraFollow.CameraMode.Zoom:
+                onSpaceEnter.AddListener(() => { 
+                    CameraFollow.mainCamera.SetMode(cameraMode);
+                    CameraFollow.mainCamera.SetZoom(zoom);
+                });
+                break;
+            case CameraFollow.CameraMode.Encapsulate:
+                onSpaceEnter.AddListener(() => { 
+                    CameraFollow.mainCamera.SetMode(cameraMode);
+                    CameraFollow.mainCamera.targets.AddRange(targets);
+                });
+                break;
+            default:
+                onSpaceEnter.AddListener(() => {
+                    CameraFollow.mainCamera.SetMode(cameraMode);
+                    CameraFollow.mainCamera.ResetZoom();
+                    CameraFollow.mainCamera.targets.Clear();
+                });
+                break;
+        }
+
+        onSpaceExit.AddListener(() => {
+            if (CameraFollow.mainCamera.currentSpace == this)
+            {
+                CameraFollow.mainCamera.SetMode(CameraFollow.CameraMode.Default);
+                CameraFollow.mainCamera.ResetZoom();
+
+                CameraFollow.mainCamera.currentSpace = null;
+            }
+
+            if (cameraMode == CameraFollow.CameraMode.Encapsulate)
+            {
+                foreach (Transform target in targets)
+                {
+                    CameraFollow.mainCamera.targets.Remove(target);
+                }
+            }
+            else if (cameraMode == CameraFollow.CameraMode.Fixed) {
+                CameraFollow.mainCamera.targets.Remove(transform);
+            }
+        });
+
+        collider = GetComponent<Collider2D>();
+        collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            CameraFollow.mainCamera.currentSpace = this;
+            onSpaceEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            onSpaceExit.Invoke();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        switch (cameraMode) {
+            case CameraFollow.CameraMode.Zoom:
+            case CameraFollow.CameraMode.Fixed:
+                Vector2 botLeft = new Vector2(transform.position.x - (16 * zoom / 9), transform.position.y - zoom);
+                Vector2 topLeft = new Vector2(transform.position.x - (16 * zoom / 9), transform.position.y + zoom);
+                Vector2 topRight = new Vector2(transform.position.x + (16 * zoom / 9), transform.position.y + zoom);
+                Vector2 botRight = new Vector2(transform.position.x + (16 * zoom / 9), transform.position.y - zoom);
+                Debug.DrawLine(botLeft, topLeft, Gizmos.color);
+                Debug.DrawLine(topLeft, topRight, Gizmos.color);
+                Debug.DrawLine(topRight, botRight, Gizmos.color);
+                Debug.DrawLine(botRight, botLeft, Gizmos.color);
+                break;
+            default:
+                break;
+        }
+    }
+}
