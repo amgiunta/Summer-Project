880cfefdaa315428b1b6aa1bfd2abb062912ae4b
Adam Giunta <amgiunta.2016@mymail.becker.edu>
2021-03-08 21:46:34 -0500

    Added gate types

--- Assets/Scripts/Switches/LogicGate.cs
+++ Assets/Scripts/Switches/LogicGate.cs
@@ -4,19 +4,108 @@ using UnityEngine;
 
 public class LogicGate : Trigger
 {
-    public enum GateType { And, Or, Nand, Nor, Xor, Not};
+    public enum GateType { And, Or, Nand, Nor, Xor};
 
     public GateType gateType;
+    public List<Trigger> inputs;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void Start() {
+        if (state == TriggerState.Inactive) {
+            Deactivate();
+        }
+        else {
+            Activate();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GateCheck() {
+        if (inputs == null) {return;}
+
+        Debug.Log("Checking gate");
+
+        switch (gateType) {
+            case GateType.Xor:
+                if (Or() && !And()) {
+                    if (state == TriggerState.Inactive)
+                        Activate();
+                }
+                else {
+                    if (state == TriggerState.Active)
+                        Deactivate();
+                }
+                return;
+            case GateType.Nor:
+                if (!Or()) {
+                    if (state == TriggerState.Inactive)
+                        Activate();
+                }
+                else {
+                    if (state == TriggerState.Active)
+                        Deactivate();
+                }
+
+                return;
+            case GateType.Or:
+                if (Or()) {
+                    if (state == TriggerState.Inactive)
+                        Activate();
+                }
+                else {
+                    if (state == TriggerState.Active)
+                        Deactivate();
+                }
+
+                return;
+            case GateType.Nand:
+                if (!And()) {
+                    if (state == TriggerState.Inactive)
+                        Activate();
+                }
+                else {
+                    if (state == TriggerState.Active)
+                        Deactivate();
+                }
+                return;
+            case GateType.And:
+                if (And()) {
+                    if (state == TriggerState.Inactive)
+                        Activate();
+                }
+                else {
+                    if (state == TriggerState.Active)
+                        Deactivate();
+                }
+                return;
+            default:
+                break;
+        }
+    }
+
+    private bool And() {
+        foreach (Trigger input in inputs) {
+            if (input.state == TriggerState.Inactive) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool Or() {
+        foreach (Trigger input in inputs) {
+            if (input.state == TriggerState.Active) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public override void Activate()
     {
-        
+        base.Activate();
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
     }
 }
